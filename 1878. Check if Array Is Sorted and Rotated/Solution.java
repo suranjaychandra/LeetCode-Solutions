97e407d73872bbceb6c32c3582ccab7569655b8b
suranjay kumar chandrawanshi <cssuranjaykumar@gmail.com>
2025-02-16 08:29:12 -0800

    Java Solution
          Submission Statistics:
          Question #: 1878
          Language: Java
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 41.108 MB
          Memory percentile: 81.89

---
+++ 1878. Check if Array Is Sorted and Rotated/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Solution {
+    public static boolean check(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > nums[(i + 1) % nums.length]) {
+                count++;
+            }
+            if (count > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print(check(arr));
+        sc.close();
+    }
+}
